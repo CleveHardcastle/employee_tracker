9d5f2c19d3d6833d4058c60f8e3c6c6711ee1c12
CleveHardcastle <treyhardcastle@gmail.com>
2022-12-16 21:02:11 -0800

    code

---
+++ index.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const inquirer = require("inquirer");
+const sequelize = require("./config");
+const db = require("./models");
+require("console.table");
+
+inquirer
+  .prompt([
+    {
+      type: "list",
+      choices: [
+        "View All Employees",
+        "Add Employee",
+        "Update Employee Role",
+        "View All Roles",
+        "Add Role",
+        "View All Departments",
+        "Add Department",
+      ],
+      message: "What would you like to do?",
+      name: "choice",
+    },
+  ])
+  .then((data) => {
+    let userChoice = data.choice;
+    switch (userChoice) {
+      case "View All Employees":
+        viewEmployees();
+    }
+  });
+
+function viewEmployees() {
+  db.findAllEmployees().then(([rows]) => {
+    let employees = rows;
+    console.log("\n");
+    console.table(employees);
+  });
+}
